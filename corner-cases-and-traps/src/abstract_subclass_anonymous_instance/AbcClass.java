3e706fdd7d56d47cce179acfe39bf709f0965c5e
Eugeny Korobka <kashey801@protonmail.com>
2021-10-13 20:08:09 +0300

    Initial commit

---
+++ corner-cases-and-traps/src/abstract_subclass_anonymous_instance/AbcClass.java
@@ -0,0 +1,7 @@
+package abstract_subclass_anonymous_instance;
+
+public abstract class AbcClass {
+	public void myMethod() {
+		System.out.println("Hello from abstract class!");
+	}
+}
---
+++ corner-cases-and-traps/src/abstract_subclass_anonymous_instance/App.java
@@ -0,0 +1,11 @@
+// https://stackoverflow.com/questions/13670991/can-we-instantiate-an-abstract-class
+
+package abstract_subclass_anonymous_instance;
+
+public class App {
+	public static void main(String[] args) {
+		AbcClass abcClass = new AbcClass() {}; 
+		abcClass.myMethod();
+		System.out.println(abcClass instanceof AbcClass);
+	}
+}
