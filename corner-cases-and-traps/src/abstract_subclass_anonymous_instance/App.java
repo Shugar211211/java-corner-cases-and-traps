5e680e3ed69967874eca66f335a90cfa72c35dfb
Eugeny Korobka <kashey801@protonmail.com>
2021-10-13 20:12:03 +0300

    Comment added

--- corner-cases-and-traps/src/abstract_subclass_anonymous_instance/App.java
+++ corner-cases-and-traps/src/abstract_subclass_anonymous_instance/App.java
@@ -4,6 +4,7 @@ package abstract_subclass_anonymous_instance;
 
 public class App {
 	public static void main(String[] args) {
+		// creating subclass of AbcClass
 		AbcClass abcClass = new AbcClass() {}; 
 		abcClass.myMethod();
 		System.out.println(abcClass instanceof AbcClass);
